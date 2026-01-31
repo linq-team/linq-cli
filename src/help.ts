02c727e21d18d61f1c39dc73dae7abb5fa26ff9e
Josh Flayhart <josh.flayhart@linqapp.com>
2026-01-30 22:10:23 -0600

    fix: remove redundant CLI description from banner

--- src/help.ts
+++ src/help.ts
@@ -1,9 +1,9 @@
 import { Help } from '@oclif/core';
-import { BANNER } from './lib/banner.js';
+import { LOGO } from './lib/banner.js';
 
 export default class LinqHelp extends Help {
   async showRootHelp(): Promise<void> {
-    console.log(BANNER);
+    console.log(LOGO);
     await super.showRootHelp();
   }
 }
--- src/lib/banner.ts
+++ src/lib/banner.ts
@@ -16,4 +16,3 @@ export const LOGO = `
         ::::
 `;
 
-export const BANNER = LOGO + '\n  Linq CLI for iMessage & SMS\n';
